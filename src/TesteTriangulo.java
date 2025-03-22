ae5a71a78bb2b2dc24774e90ff38a998b940a8f0
anaju88 <anajoliveira888@gmail.com>
2025-03-22 17:52:19 -0300

    atividade 3 da matéria de poo

--- src/TesteTriangulo.java
+++ src/TesteTriangulo.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.util.Scanner;
 
 public class TesteTriangulo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Triangulo t = new Triangulo();
+
+        System.out.println("Digite o valor da base do Triangulo: ");
+        float base = sc.nextFloat();
+
+        System.out.println("Digite o valor da altura do Triangulo: ");
+        float altura = sc.nextFloat();
+
+
+        Triangulo triangulo = new Triangulo(base, altura);
+        System.out.println(triangulo);
+
+        System.out.println("\nDados do Triângulo:");
+        System.out.println(triangulo.imprimeDados());
+    }
 }
+
+
+
+
+
+
+
+
