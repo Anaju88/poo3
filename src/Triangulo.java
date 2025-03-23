a0603184be00ed6ac6c9bba7adbc51d22a6fc8b3
anaju88 <anajoliveira888@gmail.com>
2025-03-23 00:03:03 -0300

    triangulo java - teste

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Triangulo {
-}
+
+        public float base;
+        public float altura;
+
+        public Triangulo() {
+
+        }
+
+        public Triangulo(float base, float altura) {
+            this.base = base;
+            this.altura = altura;
+        }
+
+        //métodos
+        float calculeArea(){
+            return (base * altura) / 2;
+        }
+
+        public String imprimeDados(){
+            return "Base: " + base + ", Altura: " + altura + ", Área: " + calculeArea();
+        }
+    }
+
